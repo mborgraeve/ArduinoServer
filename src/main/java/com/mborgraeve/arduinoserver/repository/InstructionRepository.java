f864a9efaba783bcc2725608686744de87f622c4
Matthieu Borgraeve <matthieu.borgraeve@gmail.com>
2017-12-26 19:16:46 -0500

    working with both database initialization

--- src/main/java/com/mborgraeve/arduinoserver/repository/InstructionRepository.java
+++ src/main/java/com/mborgraeve/arduinoserver/repository/InstructionRepository.java
@@ -7,5 +7,5 @@ import com.mborgraeve.arduinoserver.model.Instruction;
 
 public interface InstructionRepository extends JpaRepository<Instruction, Integer>{
 	Instruction findById(int id);
-
+// create method to find current instruction. Probably something like add an int priority to instructions and sort them out (float to be able to reorganize them ?).
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,6 @@
 --drop table INSTRUCTION if exists;
-drop table INSTRUCTION2 if exists;
+--drop table INSTRUCTION2 if exists;
 --create table INSTRUCTION2 (id INT primary key, instructedTemperature float DEFAULT 10.0, defaultTemperature float DEFAULT 10.0, timeLimit timestamp DEFAULT sysdate, cycleDuration int DEFAULT 60, test int default 5);
+
+drop table INSTRUCTION if exists;
+create table INSTRUCTION (id INT primary key auto_increment, instructedTemperature float DEFAULT 10.0, defaultTemperature float DEFAULT 10.0, timeLimit timestamp DEFAULT sysdate, cycleDuration int DEFAULT 60);
